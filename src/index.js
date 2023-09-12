9a0a69de9e9915e233a3c1dd67c6d5fa3b8a1577
vuongquangminh <vuongquangminh120802@gmail.com>
2023-09-12 13:20:17 +0700

    Routes & Controllers

--- src/index.js
+++ src/index.js
@@ -6,12 +6,19 @@ const handlebars = require('express-handlebars').engine;
 const app = express();
 const port = 3000;
 
+const route = require('./routes')
+
 //  cài đặt đường dẫn đến các file tính như html, css, js, img,...
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(express.json())
+
 
 // HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 //template engine 
 app.engine('hbs', handlebars({
@@ -21,13 +28,10 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'))
 
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
+// Routes init
+route(app)
 
-app.get('/new', (req, res) => {
-  res.render('new');
-})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
