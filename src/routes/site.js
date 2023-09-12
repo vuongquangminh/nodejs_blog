caa04d555fe9a35be76305c951f83f10dfc76976
vuongquangminh <vuongquangminh120802@gmail.com>
2023-09-12 13:24:28 +0700

    fix SiteController

--- src/routes/site.js
+++ src/routes/site.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const newsController = require('../app/controllers/NewsController')
+const siteController = require('../app/controllers/SiteController')
 
-router.use('/:slug', newsController.show)
-router.use('/', newsController.index)
+router.use('/search', siteController.search)
+router.use('/', siteController.index)
 
 
 module.exports = router;
